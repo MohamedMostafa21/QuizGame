da048fa4d6f378e7bd641b9ea1bc3ee7e8bd3b37
Abd-Elrahman <abdelrahmanmohammed1992003@gmail.com>
2026-03-25 19:51:47 +0200

    Adding Authentication Features

---
+++ QuizGame/Services/UserService.cs
@@ -0,0 +1,17 @@
+﻿using QuizGame.Models;
+using QuizGame.ViewModels;
+
+namespace QuizGame.Services
+{
+    public class UserService
+    {
+        public User GetUserFromRegisterViewModel(RegisterViewModel vm)
+        {
+            return new User
+            {
+                UserName = vm.Username,
+                Email = vm.Email
+            };
+        }
+    }
+}
